7d131184154f5571f32ce388d245e9ef51ad2c69
khan <khan@192.168.0.14>
2019-12-28 20:00:44 +0530

    addition progm

---
+++ demo/src/pack/Addition.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class Addition {
+	
+	public static int add(int a , int b) {
+		int k = a+b;
+		return k;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = add(10,20);
+		System.out.println("addition of two number :"+a);
+
+	}
+
+}
